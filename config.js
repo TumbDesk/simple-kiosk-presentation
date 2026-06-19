c4217184b1c3e4682946ab023a51a7ebdf1f75d5
Pedro Cewe-Woyth <pedro.cewe-woyth@redefy.net>
2026-06-19 09:08:54 +0200

    optimize sample images for faster loading times

--- config.js
+++ config.js
@@ -15,25 +15,25 @@ const presentationData = {
         "title": "Der Schulgarten",
         "description": "Vom Urwald zur grünen Oase – danke an die Garten-AG!",
         "images": [
-          { "file": "images/garden_before.png", "label": "Vor der Arbeit", "label_color": "orange" },
-          { "file": "images/garden_step1.png", "label": "Beikraut raus", "label_color": "blue" },
-          { "file": "images/garden_step2.png", "label": "PROJEKT-HIGHLIGHT", "label_color": "yellow" },
-          { "file": "images/garden_after.png", "label": "FERTIG!" }
+          { "file": "images/garden_before.jpg", "label": "Vor der Arbeit", "label_color": "orange" },
+          { "file": "images/garden_step1.jpg", "label": "Beikraut raus", "label_color": "blue" },
+          { "file": "images/garden_step2.jpg", "label": "PROJEKT-HIGHLIGHT", "label_color": "yellow" },
+          { "file": "images/garden_after.jpg", "label": "FERTIG!" }
         ]
       },
       {
         "title": "Das neue Insektenhotel",
         "description": "Vom Brett zur Luxusherberge",
         "images": [
-          { "file": "images/insecthotel_before.png", "label": "Bausatz", "label_color": "blue" },
-          { "file": "images/insecthotel_after.png", "label": "EINZUG", "label_color": "green" }
+          { "file": "images/insecthotel_before.jpg", "label": "Bausatz", "label_color": "blue" },
+          { "file": "images/insecthotel_after.jpg", "label": "EINZUG", "label_color": "green" }
         ]
       },
       {
         "title": "Die Video-AG",
         "description": "Unser stolzes Dokumentations-Team.",
         "images": [
-          { "file": "images/team.png", "label": "DAS TEAM" }
+          { "file": "images/team.jpg", "label": "DAS TEAM" }
         ]
       }
     ]
